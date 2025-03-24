68affb74d29606dcc216098558be3479502ae3f3
Sunhee Cho <ihateundefined@gamil.com>
2025-03-24 18:22:33 +0900

    dailyTask

---
+++ 250324/dailyTask.js
@@ -0,0 +1,26 @@
+/*
+사용자로부터 입력받은 숫자에 해당하는 구구단을 출력하시오.
+prompt로 입력 받은 값은 문자열이므로 숫자로 변환하기 위해서는 Number 를 사용한다. 
+var a = Number(b);
+*/
+
+/*
+const cl = console.log;
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("출력하고자 하는 단을 입력하세요 >>> ", function (dan) {
+  var numb = Number(dan);
+
+  cl("*** " + numb + "단을 출력합니다 ***");
+  for (var i = 1; i < 10; i++) {
+    cl(numb + " * " + i + " = " + numb * i);
+  }
+
+  rl.close();
+});
+*/
